5c3e5804ecd1f7241355c27eba1e6db68914d408
yair-hb <94722419+yair-hb@users.noreply.github.com>
2021-11-19 18:03:30 -0600

    Update cdo.py

--- cdo.py
+++ cdo.py
@@ -1,2 +1,17 @@
 import cv2
 import numpy as np
+import imutils
+
+cap = cv2.VideoCapture('autos.mp4')
+
+while True:
+  ret, frame = cap.read()
+  if ret == False: break
+    
+  cv2.imshow('Frame', frame)
+  
+  k= cv2.waitkey(1) & 0xff
+  if k == 27:
+    break
+cap.release()
+cv2.destroyAllWindows()
