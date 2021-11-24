3e71ed301269e7d62bf3b3c4a7a03acc40396d1b
yair <aythb@hotmail.com>
2021-11-23 18:08:13 -0600

    se completa el codigo para la detecion de movimiento (ddm.py)

--- ddm/ddm.py
+++ ddm/ddm.py
@@ -1,14 +1,30 @@
 #este es el codigo para el detector de movimiento
-import cv2;
+import cv2
 import numpy as np
 
 video = cv2.VideoCapture(0)
-
+i = 0
 while True:
 	ret, frame = video.read()
 	if ret == False: break
+	gray = cv2.cvtcolor(frame, cv2.COLOR_BGR2GRAY)
+	if i == 20:
+		bgGray = gray
+	if i > 20:
+		dif  = cv2.absdiff(gray, bgGray)
+		_, th = cv2.threshold(dif, 40, 255, cv2.THRESH_BINARY)
+		cnts, _ = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		cv2.drawContours(frame, cnts, -1, (0,0,255), 1)
+		cvs.imshow('dif', dif)
+		cvs.imshow('th', th)
+		for c in cnts:
+			area = cv2.contourArea(c)
+			if area >= 9000:
+				x,y,w,h = cv2.boundingRect(c)
+				cv2.rectangle(frame, (x,x), (x+y, y+h), (255,0,0),2)
 
 	cv2.imshow('Frame', frame)
+	i = i+1
 	if cv2.waitKey(1) & 0xff == ord ('q'):
 		break
 video.release()
